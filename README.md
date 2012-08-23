5aee0adfc62cb030f7866a33f70c22889e42f4f0
Joshua Mervine <joshua@mervine.net>
2012-08-23 03:06:33 -0400

    added initial code and readme

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Add ShareThis to NestaCMS.
 
+> Warning, this shouldn't be considered tested or final 
+> until a gem has been pushed to rubygems.org.
+
+
 ## Installation
 
 Add this line to your application's Gemfile:
@@ -18,7 +22,53 @@ Or install it yourself as:
 
 ## Usage
 
-> TODO
+1. Add share this configs.
+
+    # file: config/config.yml
+    sharethis: SHARETHIS_PARTENT_ID
+
+    sharethis_buttons: # optional
+    - :twitter
+    - :linkedin
+    - :reddit
+    - :digg
+    - :facebook_like
+    # the aboce are the defaults
+
+> IMPORTANT: Notice the above sharethis_buttons are all Symbols
+> -- meaning, they start with a colon (:) -- which is required.
+>
+> See lib/nesta-plugin-sharethis/sharethis.yml for a full list.
+> 
+> If you would like to add more, fork, add and submit a pull request.
+
+
+2. Add the 'sharethis_javascript' helper to your your layout.haml:
+
+    -#- file: views/layout.haml
+    <!DOCTYPE html>
+     %html(lang="en")
+      %head
+        %meta(charset="utf-8")
+        - if @description
+          %meta(name="description" content=@description)
+        - if @keywords
+          %meta(name="keywords" content=@keywords)
+        = sharethis_javascript
+        %title= @title
+
+    -#- ... rest of layout.haml ...
+
+
+3. Add the 'sharethis_buttons' helper to your layout, where you want
+   your buttons to appear.
+
+    -#- file: views/layout.haml
+    
+    -#- ... rest of layout.haml ...
+    = sharethis_buttons
+    -#- ... rest of layout.haml ...
+
 
 ## Contributing
 
--- lib/nesta-plugin-sharethis/init.rb
+++ lib/nesta-plugin-sharethis/init.rb
@@ -2,12 +2,62 @@ module Nesta
   module Plugin
     module Sharethis
       module Helpers
-        # If your plugin needs any helper methods, add them here...
+        def sharethis_javascript
+          %{
+          <script type="text/javascript">var switchTo5x=true;</script>
+          <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
+          <script type="text/javascript">stLight.options({publisher: "#{Nesta::Config.sharethis}"}); </script>
+          }
+        end
+
+        def sharethis_buttons
+          buttons = []
+          Nesta::Config.sharethis_buttons.each do |button|
+            buttons << self.button.send button 
+          end
+          buttons.join("\n")
+        end
       end
+
+      # Load yaml file with share this mappings.
+      SHARETHIS = YAML.load_file( File.expand_path( "sharethis.yml", File.dirname(__FILE)) )
+
+      # Access button mappings.
+      #
+      # @return [Struct] button mappings.
+      def self.button
+        unless @button
+          @button = Struct.new "Button", *(SHARETHIS.keys)
+          SHARETHIS.each do |key, val|
+            @button.send key, val
+          end
+        end
+        @button
+      end
+
     end
   end
 
   class App
     helpers Nesta::Plugin::Sharethis::Helpers
   end
+
+  class Config
+    @settings =+ %w[ sharethis sharethis_buttons ]
+
+    def sharethis_buttons
+      from_yaml("sharethis_buttons") || default_buttons
+    end
+
+    protected
+    def default_buttons
+      %w[
+        twitter
+        linkedin
+        reddit
+        facebook_like
+      ]
+    end
+  end
 end
+
